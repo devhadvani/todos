a1d5af722c6adf30f1ce02ac857e672a812a1e98
devhadvani <65004285+devhadvani@users.noreply.github.com>
2021-07-13 21:32:26 +0530

    change

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0%)txmqel7*v0&6*w@@#8(q6d%bw4h1tg+sx$$%73h3wx-8qj_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','tododevs.heroku.com']
+ALLOWED_HOSTS = ['127.0.0.1','tododevs.herokuapp.com']
 
 
 # Application definition
